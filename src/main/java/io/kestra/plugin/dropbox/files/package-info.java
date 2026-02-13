4bb4301611a5a7d670ada9583850b3bf8dbe0953
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 12:30:31 +0100

    chore: update plugin categories (#17)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

--- src/main/java/io/kestra/plugin/dropbox/files/package-info.java
+++ src/main/java/io/kestra/plugin/dropbox/files/package-info.java
@@ -1,7 +1,7 @@
 @PluginSubGroup(
     title = "Dropbox Files",
     description = "This sub-group contains tasks for managing files and folders in Dropbox.",
-    categories = { PluginSubGroup.PluginCategory.STORAGE }
+        categories = { PluginSubGroup.PluginCategory.BUSINESS }
 )
 package io.kestra.plugin.dropbox.files;
 
